ded3d9583499c53dc39e809bfb4796c614ce617a
TatyanaSimchenko <misstaty8181@gmail.com>
2021-09-22 21:13:36 +0300

    first fix 01-zoom-homework

--- 01-zoom-homework/main.js
+++ 01-zoom-homework/main.js
@@ -1,75 +1,58 @@
-console.log("Hello!");
-
 const fruit = 15.678;
-
 const coffe = 123.965;
-
 const meat = 90.2345;
 
-// 1 Максимальне число:
-let maxPrise = Math.max(15.678, 123.965, 90.2345);
+// Максимальне число:
+const maxPrise = Math.max(15.678, 123.965, 90.2345);
 console.log(maxPrise);
 
-// 2 Мінімальне число:
-let minPrise = Math.min(15.678, 123.965, 90.2345);
+// Мінімальне число:
+const minPrise = Math.min(15.678, 123.965, 90.2345);
 console.log(minPrise);
 
-// 3 Вартість всіх товарів:
-let sumPrice = fruit + coffe + meat;
+// Вартість всіх товарів:
+const sumPrice = fruit + coffe + meat;
 console.log(sumPrice);
 
-// 4 Округлення в меньшу сторону:
-let floorSumPrise = Math.floor(fruit + coffe + meat);
+//  Округлення в меньшу сторону:
+const floorSumPrise = Math.floor(fruit) + Math.floor(coffe) + Math.floor(meat);
 console.log(floorSumPrise);
 
-// 5 Сума округлена до сотень:
-let roundToSumPrise = Math.round((fruit + coffe + meat) / 100) * 100;
+//  Сума округлена до сотень:
+let roundToSumPrise = Math.round(sumPrice / 100) * 100;
 console.log(roundToSumPrise);
 
-// 6 Парне чи не парне:
+// Парне чи не парне:
 if (roundToSumPrise % 2 == 0) {
   console.log("even!");
 } else {
   console.log("odd!");
 }
 
-// 7 Сума решти:
-let delivery = (500 - (fruit + coffe + meat)).toFixed(2);
+//  Сума решти:
+let delivery = 500 - sumPrice;
 console.log(delivery);
 
-// Середнє значення:
-let averageSum = ((fruit + coffe + meat) / 3).toFixed(2);
+//  Середнє значення:
+let averageSum = (sumPrice / 3).toFixed(2);
 console.log(averageSum);
 
-// 8 Випадкова знижка:
-function discount(max, min) {
-  return min + Math.random() * (max - min);
-}
-console.log(discount(1, 10));
-
-let sumDiscount = (500 - discount(1, 10)).toFixed(2);
+//  Випадкова знижка:
+const discount = Math.floor(Math.random() * 101);
+const sumDiscount = (sumPrice - sumPrice * (discount / 100)).toFixed(2);
 console.log(sumDiscount);
 
-// 9 Чистий прибуток:
-
-const price = 260.66;
-
-const clientCount = 10;
-
-let clientCost = (Math.floor(price) * clientCount) / 100;
+//  Чистий прибуток:
+let clientCost = (Math.floor(sumPrice) * discount) / 100;
 console.log(clientCost);
-let costPrice = Math.floor(price) / 2;
-let profit = costPrice - clientCost;
-
+let costPrice = Math.floor(sumPrice) / 2;
+let profit = (costPrice - clientCost).toFixed(2);
 console.log(profit);
 
 // Шаблонний рядок:
-
 let button = document.getElementById("btn");
 let div = document.getElementById("out");
-
 button.onclick = function () {
-  console.log("good job!");
   div.innerHTML = `Максимальне число:  ${maxPrise} <br/>\n
   Мінімальне число: ${minPrise} <br/>\n
   Вартість всіх товарів: ${sumPrice} <br/>\n
