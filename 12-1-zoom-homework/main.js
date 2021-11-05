90b2ba5ce82193159ee1421b30597d5f00e74300
TatyanaSimchenko <misstaty8181@gmail.com>
2021-11-06 00:29:44 +0200

    added none style work

--- 12-1-zoom-homework/main.js
+++ 12-1-zoom-homework/main.js
@@ -4,6 +4,16 @@ const baseUrl = "https://swapi.dev/api";
 const filmList = document.getElementById("film-list");
 const filmBtn = document.getElementById("people-btn");
 const filmInput = document.getElementById("input");
+const filmInfo = document.getElementById("film-info");
+const aboutBtn = document.getElementById("about-btn");
+const aboutInput = document.getElementById("about-input");
+const planetsBtn = document.getElementById("planets-btn");
+const planetsList = document.getElementById("planets-list");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const countPage = document.getElementById("count-page");
+
+// personages info, after choose part of film
 
 const getPeople = () => {
   if (
@@ -57,6 +67,73 @@ const getInfo = (link) => {
     });
 };
 
+// About films info
+
+const getFilm = () => {
+  if (
+    aboutInput.value === "1" ||
+    aboutInput.value === "2" ||
+    aboutInput.value === "3" ||
+    aboutInput.value === "4" ||
+    aboutInput.value === "5" ||
+    aboutInput.value === "6"
+  ) {
+    filmInfo.innerHTML = "Loading...";
+  } else {
+    filmInfo.innerHTML = "Please, enter from 1 to 6!";
+    return;
+  }
+  axios
+    .get(`${baseUrl}/films/${aboutInput.value}`)
+    .then((response) => {
+      filmInfo.innerHTML = `
+      <div class="about__box">
+      <h2>${response.data.title}</h2>
+      <p>Release date: ${response.data.release_date}</p>
+      <p>Producer: ${response.data.Producer}</p>
+      <p>Episode: ${response.data.episode_id}</p>
+      </div>`;
+    })
+    .catch((err) => {
+      console.log("Something Wrong!", err);
+    });
+};
+
+// Planets list
+let currentPage = 1;
+const getPlanets = () => {
+  planetsList.innerHTML = "Loading...";
+  countPage.innerHTML = currentPage;
+  axios
+    .get(`${baseUrl}/planets/?page=${currentPage}`)
+    .then((response) => {
+      const elems = response.data.results.map(
+        (item) => `<li class="planets-item">${item.name}</li>`
+      );
+      planetsList.innerHTML = elems.join("");
+    })
+    .catch((err) => {
+      console.log("Something Wrong!", err);
+    });
+};
+
 getPeople;
+getFilm;
+getPlanets;
 
 filmBtn.addEventListener("click", getPeople);
+aboutBtn.addEventListener("click", getFilm);
+planetsBtn.addEventListener("click", getPlanets);
+
+// prev & next buttons
+
+prevBtn.addEventListener("click", () => {
+  if (currentPage === 1) return;
+  currentPage -= 1;
+  getPlanets();
+});
+nextBtn.addEventListener("click", () => {
+  if (currentPage === 6) return;
+  currentPage += 1;
+  getPlanets();
+});
