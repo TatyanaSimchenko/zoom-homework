35bdf290a65e999a55f3060a87bc38f094722eed
TatyanaSimchenko <misstaty8181@gmail.com>
2021-11-10 10:44:13 +0200

    fix some in js & css

--- 12-1-zoom-homework/main.js
+++ 12-1-zoom-homework/main.js
@@ -12,19 +12,13 @@ const planetsList = document.getElementById("planets-list");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 const countPage = document.getElementById("count-page");
+const overlay = document.querySelector(".overlay");
 
 // Personages info, after choose part of film
 
 const getPeople = () => {
-  if (
-    filmInput.value === "1" ||
-    filmInput.value === "2" ||
-    filmInput.value === "3" ||
-    filmInput.value === "4" ||
-    filmInput.value === "5" ||
-    filmInput.value === "6"
-  ) {
-    document.querySelector(".overlay").classList.add("active");
+  if (Number(filmInput.value) >= 1 && Number(filmInput.value) <= 6) {
+    overlay.classList.add("active");
   } else {
     filmList.innerHTML = `<p class="answer">Please, enter from 1 to 6!</p>`;
     return;
@@ -61,7 +55,7 @@ const getInfo = (link) => {
     <p class="card__subtitle">Gender: ${gender}</p>
     </div>`;
       filmList.innerHTML += allPersonInfo;
-      document.querySelector(".overlay").classList.remove("active");
+      overlay.classList.remove("active");
     })
     .catch((err) => {
       console.log("Something Wrong!", err);
@@ -71,15 +65,8 @@ const getInfo = (link) => {
 // About films info
 
 const getFilm = () => {
-  if (
-    aboutInput.value === "1" ||
-    aboutInput.value === "2" ||
-    aboutInput.value === "3" ||
-    aboutInput.value === "4" ||
-    aboutInput.value === "5" ||
-    aboutInput.value === "6"
-  ) {
-    document.querySelector(".overlay").classList.add("active");
+  if (Number(aboutInput.value) >= 1 && Number(aboutInput.value) <= 6) {
+    overlay.classList.add("active");
   } else {
     filmInfo.innerHTML = `<p class="answer">Please, enter from 1 to 6!</p>`;
     return;
@@ -94,7 +81,7 @@ const getFilm = () => {
       <p class="card__subtitle">Producer: ${response.data.Producer}</p>
       <p class="card__subtitle">Episode: ${response.data.episode_id}</p>
       </div>`;
-      document.querySelector(".overlay").classList.remove("active");
+      overlay.classList.remove("active");
     })
     .catch((err) => {
       console.log("Something Wrong!", err);
@@ -106,7 +93,7 @@ const getFilm = () => {
 let currentPage = 1;
 
 const getPlanets = () => {
-  document.querySelector(".overlay").classList.add("active");
+  overlay.classList.add("active");
   countPage.innerHTML = currentPage;
   axios
     .get(`${baseUrl}/planets/?page=${currentPage}`)
@@ -115,16 +102,16 @@ const getPlanets = () => {
         (item) => `<li class="planets-item">${item.name}</li>`
       );
       planetsList.innerHTML = elems.join("");
-      document.querySelector(".overlay").classList.remove("active");
-      document.querySelector(".prev").classList.add("active");
-      document.querySelector(".next").classList.add("active");
+      overlay.classList.remove("active");
+      prevBtn.classList.add("active");
+      nextBtn.classList.add("active");
       if (currentPage === 6) {
-        document.querySelector(".prev").classList.add("active");
-        document.querySelector(".next").classList.remove("active");
+        prevBtn.classList.add("active");
+        nextBtn.classList.remove("active");
       }
       if (currentPage === 1) {
-        document.querySelector(".prev").classList.remove("active");
-        document.querySelector(".next").classList.add("active");
+        prevBtn.classList.remove("active");
+        nextBtn.classList.add("active");
       }
     })
     .catch((err) => {
@@ -144,10 +131,6 @@ nextBtn.addEventListener("click", () => {
   currentPage += 1;
   getPlanets();
 });
-
-getPeople;
-getFilm;
-getPlanets;
 
 filmBtn.addEventListener("click", getPeople);
 aboutBtn.addEventListener("click", getFilm);
